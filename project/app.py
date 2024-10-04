f17d400dc4e1f896db02b7b76c19c1714425a7de
Ryan Alizadeh <ryancalizadeh@gmail.com>
2024-10-04 13:20:21 -0400

    update requirements.txt

--- project/app.py
+++ project/app.py
@@ -104,5 +104,5 @@ def delete_entry(post_id):
 
 if __name__ == "__main__":
     import os
-    PORT = os.environ.get("PORT", 5000)
+    PORT = os.environ.get("PORT", 5002)
     app.run(host="0.0.0.0", port=PORT)
