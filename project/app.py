574c82bc719ab718e0aabf951ea1bd4f9f3cf1df
Ryan Alizadeh <ryancalizadeh@gmail.com>
2024-10-04 12:24:12 -0400

    made some changes

---
+++ project/app.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+from flask import Flask, g, render_template
+
+# configuration
+DATABASE = "flaskr.db"
+
+# create and initialize a new Flask app
+app = Flask(__name__)
+
+# load the config
+app.config.from_object(__name__)
+
+# connect to database
+def connect_db():
+    """Connects to the database."""
+    rv = sqlite3.connect(app.config["DATABASE"])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+# create the database
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource("schema.sql", mode="r") as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+# open database connection
+def get_db():
+    if not hasattr(g, "sqlite_db"):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+# close database connection
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, "sqlite_db"):
+        g.sqlite_db.close()
+
+@app.route('/')
+def index():
+    """Searches the database for entries, then displays them."""
+    db = get_db()
+    cur = db.execute('select * from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('index.html', entries=entries)
+
+
+if __name__ == "__main__":
+    app.run()
